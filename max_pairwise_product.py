05c9e6602248f6cd655c086271cb1cb7bfd65a77
anastuckner <56557967+anastuckner@users.noreply.github.com>
2021-07-15 15:33:21 -0400

    Create max_pairwise_product.py

---
+++ max_pairwise_product.py
@@ -0,0 +1,59 @@
+import random
+
+
+def MaxPPFast(n, a):
+   
+    # n = int(input())
+    # a = [int(x) for x in input().split()]
+    # assert(len(a) == n)
+       
+    result1 = 0
+   
+    a.sort()
+
+   
+    result1=a[n-1]*a[n-2]
+    return result1
+
+def MaxPPNaive(n, a):
+   
+    # n = int(input())
+    # a = [int(x) for x in input().split()]
+    # assert(len(a) == n)
+   
+    result2 = 0
+   
+    for i in range(n):
+        for j in range(i+1, n):
+            
+            if a[i]*a[j] > result2:
+                result2 = a[i]*a[j]
+            
+   
+    return result2
+
+
+def StressTest(n, m):
+  while True:
+   
+    n = random.randint(2,n)
+   
+    a = [None]*n
+   
+    for i in range(n):
+       a[i] = random.randint(0,m)
+
+    
+    result1 = MaxPPNaive(n, a)
+    result2 = MaxPPFast(n, a)
+   
+    if result1==result2:
+       
+          print('OK')
+   
+    else:
+       
+        print('Wrong answer:',result1,result2)
+        return 
+      
+StressTest(500, 5000)
